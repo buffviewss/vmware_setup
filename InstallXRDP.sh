f650eb5e40d67a42060d2f6cc688184bf61f89e6
buffviewss <buff.viewss@gmail.com>
2025-08-16 15:56:32 +0700

    Update InstallXRDP.sh

--- InstallXRDP.sh
+++ InstallXRDP.sh
@@ -1,7 +1,20 @@
+# Cài đủ gói cần thiết
 sudo apt update
-sudo apt install -y xrdp xorgxrdp
-# Bật kết nối qua Hyper-V socket (vsock)
+sudo apt install -y xrdp xorgxrdp gvfs-fuse
+
+# (QUAN TRỌNG) Cho user 'xrdp' vào group ssl-cert để đọc key TLS
+sudo adduser xrdp ssl-cert
+
+# Bật kênh Hyper-V socket (vsock)
 sudo sed -i 's/^port=.*/port=-1/' /etc/xrdp/xrdp.ini
 sudo sed -i 's/^#\?use_vsock=.*/use_vsock=true/' /etc/xrdp/xrdp.ini
-sudo adduser $USER ssl-cert
+
+# Nạp module hv_sock và tự nạp sau reboot
+echo hv_sock | sudo tee /etc/modules-load.d/hv_sock.conf
+sudo modprobe hv_sock
+
+# Khởi động dịch vụ và đặt auto-start
 sudo systemctl enable --now xrdp
+
+# Kiểm tra trạng thái (phải thấy active (running))
+systemctl status xrdp --no-pager -l
